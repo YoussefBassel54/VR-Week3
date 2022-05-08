28a5ff9b7fe1d4c6377d3fe10ed451654a220887
YoussefBassel54 <youssefbassel54@gmail.com>
2022-05-08 10:05:55 +0200

    Initial commit

---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float time = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            Instantiate(enemy, new Vector3(Random.Range(571.18f, 589.05f), 0, Random.Range(381.55f, 403.52f)), enemy.transform.rotation);
+            time = 2;
+        }
+    }
+}
---
+++ Assets/scripts/shooter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject shootingOffset;
+    public AudioSource gunshot;
+    float time = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject currentBullet = Instantiate(bullet, shootingOffset.transform.position, shootingOffset.transform.rotation);
+            currentBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 100, ForceMode.Impulse);
+            gunshot.Play();
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                Destroy(currentBullet);
+                time = 2;
+            }
+        }
+
+        
+    }
+}
